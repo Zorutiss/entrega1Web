9c3bb8c0a37f297071235528cb91272817a85197
vicgalsan1@gmail.com <vicga@VicTorrija.localdomain>
2025-11-12 20:25:01 +0100

    Prueba de la barra de busqueda

--- entrega1Web/src/components/Navbar.jsx
+++ entrega1Web/src/components/Navbar.jsx
@@ -1,14 +1,36 @@
-import {} from 'react'
+import {useState} from 'react'
 
 export default function Navbar(){
 
+    
+    const [textoBusqueda, setTextoBusqueda] = useState("");
+
+    const handleChange = (event) => {
+        setTextoBusqueda(event.target.value);
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log("Texto buscado:", textoBusqueda);
+    }
+
     return(
     <nav>
-         <ul>
-            <li>BUSCAR: </li>
-        </ul>
+        <div className = "logoNavBar">
+            <h1>TV Maze</h1>
+        </div>
+        
+
+        <form className = "busqueda" onSubmit={handleSubmit}>
+        <input
+            type = "text"
+            placeholder = "Introduce el nombre de una película"
+            value = {textoBusqueda}
+            onChange = {handleChange}
+        />
+        <button type = "submit">Buscar</button>
+        </form>
+
     </nav>  
     )
-   
-
 }
