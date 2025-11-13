7200c150b177176414ee9f0ae431fd5b9182f567
vicgalsan1@gmail.com <vicga@VicTorrija.localdomain>
2025-11-13 18:45:56 +0100

    Pruebas del fetch de la pagina

--- entrega1Web/src/App.jsx
+++ entrega1Web/src/App.jsx
@@ -2,8 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 import Navbar from './components/Navbar.jsx'
+
 function App() {
 
+  const [results, setResults] = useState([]);
+  const [selectedShow, setSelectedShow] = useSTate(null);
+  const handleSearch = async (query) => {
+    const apiRes = await fetch(`http://api.tvmaze.com/search/shows?q=${query}`)
+  };
   return (
       <div>
         <Navbar/>
