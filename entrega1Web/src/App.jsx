056e5258b451234ed967572cbe4f31736d55117d
vicgalsan1@gmail.com <vicga@VicTorrija.localdomain>
2025-11-13 18:54:31 +0100

    Mas pruebas de la API

--- entrega1Web/src/App.jsx
+++ entrega1Web/src/App.jsx
@@ -5,11 +5,31 @@ import Navbar from './components/Navbar.jsx'
 
 function App() {
 
+  //Para almacenar el resultado de la API
   const [results, setResults] = useState([]);
   const [selectedShow, setSelectedShow] = useSTate(null);
   const handleSearch = async (query) => {
-    const apiRes = await fetch(`http://api.tvmaze.com/search/shows?q=${query}`)
+    const apiRes = await fetch(`http://api.tvmaze.com/search/shows?q=${query}`);
+
+    //Comprobamnos que se puede conectar
+    if(!apiRes.ok){
+      throw new Error("ERROR AL CONECTARSE A LA API"); 
+    }
+
+  //Hacemos un try catch para asegurarnos de que
+  // funciona la API
+  try{
+    const data = await apiRes.json();
+    console.log(data);
+    setResults(data.map(item => item.show))
+  }catch{
+    console.Error("ERROR AL RECIBIR RESPUESTA DE LA API", error);
+  }
+
   };
+
+  
+
   return (
       <div>
         <Navbar/>
