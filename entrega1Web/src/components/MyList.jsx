3c86abab0c74d6d3122a3ea52b3c9d9751fd736e
vicgalsan1@gmail.com <vicga@VicTorrija.localdomain>
2025-11-16 19:00:24 +0100

    Cambios en los botones de la lista y arreglos en la lista

--- entrega1Web/src/components/MyList.jsx
+++ entrega1Web/src/components/MyList.jsx
@@ -1,31 +1,35 @@
 import './MyList.css'
+
 export default function MyList({ myList, removeFromList }) {
   return (
-    <div className="list-panel">
-      <h2>Mi Lista</h2>
+    <div className="list-container">
+
+      <h2 className="Lista">Mi Lista</h2>
 
       {myList.length === 0 && <p>No has añadido ninguna serie aún.</p>}
 
-      {myList.map(show => (
-        <div key={show.id} className="list-item">
-          
-          {show.image && (
-            <img src={show.image.medium} alt={show.name} />
-          )}
+      <div className="list-panel">
+        {myList.map(show => (
+          <div key={show.id} className="list-item">
+            
+            {show.image && (
+              <img src={show.image.medium} alt={show.name} />
+            )}
 
-          <div>
-            <h3>{show.name}</h3>
-            <button onClick={() => removeFromList(show.id)}>
-              Eliminar
-            </button>
-          </div>
-        </div>
-        
-      ))}
-      <div className="end-list">
-                <h2></h2>
+            <div>
+              <h3>{show.name}</h3>
+              <button onClick={() => removeFromList(show.id)}>
+                Eliminar
+              </button>
             </div>
+          </div>
+        ))}
+      </div>
+
+      <div className="end-list">
+        <h2></h2>
+      </div>
+
     </div>
-    
   );
 }
